4b0ad526071b5d017a948e4bb6258ee8aa09fa8d
sosome2020 <letebelebrightsosome@gmail.com>
2020-12-03 22:48:13 +0900

    2nd commit; models,controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  resources :feeds do
+    collection do
+        post :confirm
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
