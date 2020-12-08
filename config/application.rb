0899fa88f249ccdb1dc7e94496d33de2ed9c154a
sosome2020 <letebelebrightsosome@gmail.com>
2020-12-08 12:12:37 +0900

    Setting time zone and the blocking of unnecessary generation of files

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Instagramclone
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja# Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.generators do |g|
+          # The description in these two lines creates a setting that is not automatically generated.
+          g.assets false
+          g.helper false
+        end 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
